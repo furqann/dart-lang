2358bf594cbb3efcae42db85d301d5b553b9009e
Furqan Ahmad <fahmad@stellatechnology.com>
2020-04-17 01:38:18 +0500

    varaible types in dart

---
+++ features/variables.dart
@@ -0,0 +1,22 @@
+void main(List<String> arguments) {
+  int num = 25;
+  String collegeName = "some college";
+  double price = 1.99;
+
+  const nameFirst = 'Voyager I';
+  const String nameLast = 'Voyager I';
+
+  final year = 1977;
+  final leapYear = 2020;
+
+  //Auto configure variable type
+  var antennaDiameter = 3.7;
+
+  //If varai
+  Object flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  
+  dynamic image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+}
